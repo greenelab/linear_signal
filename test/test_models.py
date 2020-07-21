06edad1f7c77ebeb05c27fd9cf5db1b203b067a1
Ben Heil <ben.jer.heil@gmail.com>
2020-07-21 13:55:33 -0400

    Test models

--- test/test_models.py
+++ test/test_models.py
@@ -4,6 +4,8 @@ import os
 
 import numpy as np
 import pytest
+import torch
+import yaml
 
 import test_datasets
 from saged import models, datasets
@@ -21,8 +23,41 @@ def sklearn_models():
 def pytorch_models():
     model_list = []
     # Pytorch models go here
+    model_list.append(create_three_layer_net())
 
     return model_list
+
+
+@pytest.fixture(scope="module")
+def config():
+    config_dict = None
+
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(test_dir, 'data', 'test_config.yml')
+    with open(config_path) as config_file:
+        config_dict = yaml.safe_load(config_file)
+
+    return config_dict
+
+
+def create_three_layer_net():
+    config = None
+
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(test_dir, 'data', 'test_config.yml')
+    with open(config_path) as config_file:
+        config = yaml.safe_load(config_file)
+
+    loss_fn = torch.nn.CrossEntropyLoss
+    optimizer = torch.optim.Adam
+    model_class = models.ThreeLayerClassifier
+
+    model = models.PytorchSupervised(config,
+                                     optimizer,
+                                     loss_fn,
+                                     model_class)
+
+    return model
 
 
 @pytest.fixture(scope="module")
@@ -45,8 +80,8 @@ def test_sklearn_fit_predict(sklearn_models, dataset):
         assert type(predictions) == np.ndarray
 
 
-def test_save_load_model(all_models, dataset):
-    for model in all_models:
+def test_sklearn_save_load_model(sklearn_models, dataset):
+    for model in sklearn_models:
         model.fit(dataset)
 
         unlabeled_dataset = datasets.RefineBioUnlabeledDataset.from_labeled_dataset(dataset)
@@ -62,9 +97,61 @@ def test_save_load_model(all_models, dataset):
         os.remove('model_test.pkl')
 
 
-def test_load_params(pytorch_models):
-    raise NotImplementedError
+def test_pytorch_save_load_model(pytorch_models, dataset, config):
+    for model in pytorch_models:
+        model.fit(dataset)
+
+        unlabeled_dataset = datasets.RefineBioUnlabeledDataset.from_labeled_dataset(dataset)
+        predictions = model.predict(unlabeled_dataset)
+
+        model.save_model('model_test.pkl')
+        # FIXME These are the same as in create_three_layer_net, but really the information should
+        # probably be stored in the pickle file somehow
+        optimizer = torch.optim.Adam
+        loss_fn = torch.nn.CrossEntropyLoss
+        model_class = models.ThreeLayerClassifier
+
+        loaded_model = type(model).load_model('model_test.pkl',
+                                              config,
+                                              optimizer,
+                                              loss_fn,
+                                              model_class)
+        # Send model to gpu if applicable
+        loaded_model.model.to(loaded_model.device)
+
+        new_predictions = loaded_model.predict(unlabeled_dataset)
+
+        assert np.array_equal(predictions.cpu(), new_predictions.cpu())
+
+        os.remove('model_test.pkl')
 
 
-def test_pytorch_fit_predict(pytorch_models, dataset):
-    raise NotImplementedError
+def test_load_params(pytorch_models, dataset):
+    for model in pytorch_models:
+        original_params = model.get_parameters()
+
+        model = model.fit(dataset)
+        new_params = model.get_parameters()
+
+        for orig_key, trained_key in zip(original_params, new_params):
+            assert not np.array_equal(original_params[orig_key].cpu(),
+                                      new_params[trained_key].cpu())
+
+        model = model.load_parameters(original_params)
+        loaded_params = model.get_parameters()
+
+        for orig_key, loaded_key in zip(original_params, loaded_params):
+            assert np.array_equal(original_params[orig_key].cpu(),
+                                  loaded_params[loaded_key].cpu())
+
+
+def test_pytorch_fit_predict(pytorch_models, dataset, config):
+    for model in pytorch_models:
+        original_params = model.get_parameters()
+
+        model = model.fit(dataset)
+        trained_params = model.get_parameters()
+
+        for orig_key, trained_key in zip(original_params, trained_params):
+            assert not np.array_equal(original_params[orig_key].cpu(),
+                                      trained_params[trained_key].cpu())
