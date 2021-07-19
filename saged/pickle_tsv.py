3af62e8f667995278bab9b836ece73cc8d3dbc3b
Ben Heil <ben.jer.heil@gmail.com>
2021-07-19 13:46:44 -0400

    Modify pickle_tsv to avoid loading whole dataset into memory

--- saged/pickle_tsv.py
+++ saged/pickle_tsv.py
@@ -1,6 +1,9 @@
 import argparse
+import gc
 
+import numpy as np
 import pandas as pd
+import tqdm
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -12,7 +15,22 @@ if __name__ == '__main__':
                         )
     args = parser.parse_args()
 
+    dtypes = {'study': str}
+
     with open(args.tsv_path) as tsv_file:
-        df = pd.read_csv(tsv_file, sep='\t', index_col=0)
+        header = tsv_file.readline()
+        header = header.strip().split('\t')[1:]
+
+        for col in header:
+            dtypes[col] = np.half  # half precision floats
+
+    chunksize = 10000
+
+    df = pd.DataFrame()
+    data_iterator = pd.read_csv(args.tsv_path, sep='\t', index_col=0, dtype=dtypes,
+                                chunksize=chunksize)
+    for sub_data in tqdm.tqdm(data_iterator, total=32):
+        df = df.append(sub_data)
+        gc.collect()
 
     df.to_pickle(args.out_path)
