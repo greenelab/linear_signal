2d8c97ef731a50feb2f9dceb704fd18314f54280
Ben Heil <ben.jer.heil@gmail.com>
2021-08-04 08:00:45 -0400

    Add converted notebook

--- notebook/converted/benchmark_results.py
+++ notebook/converted/benchmark_results.py
@@ -1092,7 +1092,7 @@ in_files = glob.glob('../../results/Blood.Breast.*.tsv')
 print(in_files[:5])
 
 
-# In[6]:
+# In[3]:
 
 
 tissue_metrics = pd.DataFrame()
@@ -1115,13 +1115,61 @@ tissue_metrics['supervised'] = tissue_metrics['supervised'].str.replace('pytorch
 tissue_metrics
 
 
-# In[8]:
+# In[4]:
 
 
 plot = ggplot(tissue_metrics, aes(x='train_count', y='balanced_accuracy', color='supervised')) 
 plot += geom_smooth()
 plot += geom_point(alpha=.2)
 plot += geom_hline(yintercept=.5, linetype='dashed')
-plot += ggtitle('Brain vs Breast Tissue Prediction')
+plot += ggtitle('Blood vs Breast Tissue Prediction')
 plot
+
+
+# ### All Tissue Predictions
+
+# In[5]:
+
+
+in_files = glob.glob('../../results/all-tissue.*.tsv')
+print(in_files[:5])
+
+
+# In[6]:
+
+
+tissue_metrics = pd.DataFrame()
+for path in in_files:
+    new_df = pd.read_csv(path, sep='\t')
+    model_info = path.strip('.tsv').split('all-tissue.')[-1]
+    model_info = model_info.split('_')
+        
+    supervised_model = '_'.join(model_info[:2])
+             
+    new_df['supervised'] = supervised_model
+    
+    new_df['seed'] = model_info[-1]
+        
+    tissue_metrics = pd.concat([tissue_metrics, new_df])
+    
+tissue_metrics['train_count'] = tissue_metrics['train sample count']
+
+tissue_metrics['supervised'] = tissue_metrics['supervised'].str.replace('pytorch_supervised', 'three_layer_net')
+tissue_metrics
+
+
+# In[9]:
+
+
+plot = ggplot(tissue_metrics, aes(x='train_count', y='balanced_accuracy', color='supervised')) 
+plot += geom_smooth()
+plot += geom_point(alpha=.2)
+plot += ggtitle('Multiclass Tissue Prediction')
+plot
+
+
+# In[ ]:
+
+
+
 
