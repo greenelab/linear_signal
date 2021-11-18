7f7efbd4d870d526719a815003079b6385afd448
Ben Heil <ben.jer.heil@gmail.com>
2021-11-18 11:33:26 -0500

    Allow predict_tissue to accept loss weights

--- saged/predict_tissue.py
+++ saged/predict_tissue.py
@@ -59,6 +59,9 @@ if __name__ == '__main__':
                         default=False, action='store_true')
     parser.add_argument('--biobert', help='Add biobert embeddings as features the model can use',
                         default=False, action='store_true')
+    parser.add_argument('--weighted_loss',
+                        help='Weight classes based on the inverse of their prevalence',
+                        action='store_true')
 
     args = parser.parse_args()
 
@@ -113,6 +116,9 @@ if __name__ == '__main__':
         labels_to_keep = [args.tissue1, args.tissue2]
 
     labeled_data.subset_samples_to_labels(labels_to_keep)
+
+    if args.weighted_loss:
+        loss_weights = utils.calculate_loss_weights(labeled_data)
 
     # Correct for batch effects
     if args.batch_correction_method is not None:
@@ -188,6 +194,7 @@ if __name__ == '__main__':
                 supervised_config = yaml.safe_load(supervised_file)
                 supervised_config['input_size'] = input_size
                 supervised_config['output_size'] = output_size
+                supervised_config['loss_weights'] = loss_weights
                 if 'save_path' in supervised_config:
                     # Append script-specific information to model save file
                     save_path = supervised_config['save_path']
