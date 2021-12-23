59e698669ed218ad4b11c5e7fbafad14a3c91961
Ben Heil <ben.jer.heil@gmail.com>
2021-12-23 13:45:20 -0500

    Add batch log skl class weight calculation

--- saged/predict_tissue.py
+++ saged/predict_tissue.py
@@ -330,6 +330,9 @@ if __name__ == '__main__':
                 if args.weighted_loss:
                     loss_weights = utils.calculate_loss_weights(train_data)
                     supervised_config['loss_weights'] = loss_weights
+                    if supervised_config['name'] == 'BatchLogRegSKL':
+                        weights = utils.calculate_skl_class_weights(train_data)
+                        supervised_config['class_weights'] = weights
                 if 'save_path' in supervised_config:
                     # Append script-specific information to model save file
                     save_path = supervised_config['save_path']
